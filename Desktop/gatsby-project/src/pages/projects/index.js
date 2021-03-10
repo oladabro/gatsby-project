da857f4758ac298d6e8959b9a2e97d1f68367b9f
Ola <oladabro@gmail.com>
2021-03-10 20:59:17 +0100

    fetch data from markdown files and list projects in index.js projects

--- Desktop/gatsby-project/src/pages/projects/index.js
+++ Desktop/gatsby-project/src/pages/projects/index.js
@@ -1,16 +1,44 @@
+import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../../components/Layout"
 import { portfolio } from "../../styles/projects.module.css"
+import { styleProjects } from "../../styles/projects.module.css"
 
-function Projects() {
+export default function Projects({ data }) {
+  const projects = data.allMarkdownRemark.nodes
+
+  console.log(projects)
   return (
     <Layout>
       <div className={portfolio}>
         <h2>Portfolio</h2>
         <h3>Projects & Websites I've Created</h3>
+        <div className={styleProjects}>
+          {projects.map(project => (
+            <Link to={"/projects" + project.frontmatter.slug} key={project.id}>
+              <div>
+                <h3>{project.frontmatter.title}</h3>
+                <p>{project.frontmatter.stack}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </Layout>
   )
 }
 
-export default Projects
+export const query = graphql`
+  query ProjectsPage {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          slug
+          stack
+          title
+        }
+        id
+      }
+    }
+  }
+`
