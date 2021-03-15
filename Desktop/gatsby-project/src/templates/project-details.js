f2c99c582e931882216bbbc6737fd106bc965af9
Ola <oladabro@gmail.com>
2021-03-15 11:31:46 +0100

    add query variable to project-details.js

--- Desktop/gatsby-project/src/templates/project-details.js
+++ Desktop/gatsby-project/src/templates/project-details.js
@@ -1,19 +1,46 @@
 import React from "react"
 import Layout from "../components/Layout"
 import Img from "gatsby-image"
-import { details, featured, html } from "../styles/project-details.module.css"
+import {
+  details,
+  featured,
+  styleHtml,
+} from "../styles/project-details.module.css"
+import { graphql } from "gatsby"
 
-function ProjectDetails() {
+export default function ProjectDetails({ data }) {
+  const { html } = data.markdownRemark
+  const { title, stack, featuredImg } = data.markdownRemark.frontmatter
+
   return (
     <Layout>
       <div className={details}>
-        <h2>title</h2>
-        <h3>stack</h3>
-        <div className={featured}>{/* <Img fluid={} /> */}</div>
-        {/* <div className={styles.html} dangerouslySetInnerHTML={} /> */}
+        <h2>{title}</h2>
+        <h3>{stack}</h3>
+        <div className={featured}>
+          <Img fluid={featuredImg.childImageSharp.fluid} />
+        </div>
+        <div className={styleHtml} dangerouslySetInnerHTML={{ __html: html }} />
       </div>
     </Layout>
   )
 }
 
-export default ProjectDetails
+export const query = graphql`
+  query ProjectDetails($slug: String) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        stack
+        title
+        featuredImg {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
